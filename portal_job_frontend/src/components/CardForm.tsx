6e914ce6b18b2f69ee7082de4e9bf568977d0681
alain-nambi <alainnambi@gmail.com>
2024-02-08 12:08:23 +0300

    fixed error on vercel

--- portal_job_frontend/src/components/CardForm.tsx
+++ portal_job_frontend/src/components/CardForm.tsx
@@ -3,7 +3,7 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
+  // CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -11,8 +11,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
   Select,
-  SelectContent,
-  SelectItem,
+  // SelectContent,
+  // SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
